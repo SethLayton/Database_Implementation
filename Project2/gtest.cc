4a39a4a58b5796726f9a2995970a7ef8110e1469
sethlayton <sethlayton@gmail.com>
2022-02-25 16:23:26 -0500

    remove some comments

--- Project2/gtest.cc
+++ Project2/gtest.cc
@@ -17,32 +17,23 @@ struct consumer_args {
 	int success_val;
 };
 
-//relation *rel_ptr[] = {n, r, c, p, ps, o, li, s}; 
-
 void *producer (void *arg) {
 
 	Pipe *myPipe = (Pipe *) arg;
 
 	Record temp;
-	int counter = 0;
 
 	DBFile dbfile;
 	dbfile.Open (rel->path ()); 
-	// cout << " producer: opened DBFile " << rel->path () << endl;
 	dbfile.MoveFirst ();
 
 	while (dbfile.GetNext (temp) == 1) {
-		counter += 1;
-		if (counter%100000 == 0) {
-			//  cerr << " producer: " << counter << endl;	
-		}
 		myPipe->Insert (&temp);
 	}
 
 	dbfile.Close ();
 	myPipe->ShutDown ();
 
-	// cout << " producer: inserted " << counter << " recs into the pipe\n";
 	pthread_exit(NULL);
 }
 
@@ -82,22 +73,15 @@ void *consumer (void *arg) {
 		i++; 
 	}
  
-	//cout << " consumer: removed " << i << " recs from the pipe\n";
- 
 	if (t->write) {
 		if (last) {
 			dbfile.Add (*last);
 		}
-		//cerr << " consumer: recs removed written out as heap DBFile at " << outfile << endl;
 		dbfile.Close ();
 	}
-	// cerr << " consumer: " << (i - err) << " recs out of " << i << " recs in sorted order \n";
 	if (err) {
-		// cerr << " consumer: " <<  err << " recs failed sorted order test \n" << endl;
-		// cout << "set tutil-> success to 0" << endl;
 		t->success = 0;
 	} else {
-		// cout << "set tutil->success to 1" << endl;
 		t->success=1;
 	}
 	
@@ -105,11 +89,10 @@ void *consumer (void *arg) {
 
 }
 
-int initAllDBFiles () {  
+int initAllDBFiles () {
 
 		relation *rel_ptr[] = {n, r, c, p, ps, o, li, s};
 		struct stat buffer;   
-  		
 		for (int i = 0; i < 8; i++){
 			DBFile dbfile;
 			rel = rel_ptr[i];
@@ -117,8 +100,7 @@ int initAllDBFiles () {
 			if (stat (name.c_str(), &buffer) == 0) { 
 				cout << " DBFile already exists. Loading. " << rel->path () << endl;
 			}
-			else {
-								
+			else {				
 				cout << " DBFile will be created at " << rel->path () << endl;
 				dbfile.Create (rel->path(), heap, NULL);				
 			}
@@ -143,10 +125,8 @@ int test1 (int option, int runlen,int table, char* colname) {
 	rel = rel_ptr[table];
 
 	// sort order for records
-	// cout << "trying to create ordermaker" << endl;
 	OrderMaker sortorder (rel->schema()); 
 	rel->get_sort_order (sortorder, colname);
-	// cout << "got sortorder" << endl;
 	int buffsz = 100; // pipe cache size
 	Pipe input (buffsz);
 	Pipe output (buffsz);
@@ -154,7 +134,6 @@ int test1 (int option, int runlen,int table, char* colname) {
 	// thread to dump data i nto the input pipe (for BigQ's consumption)
 	pthread_t thread1;
 	pthread_create (&thread1, NULL, producer, (void *)&input);
-	// cout << "finished creating producer" << endl;
 	// thread to read sorted data from output pipe (dumped by BigQ)
 	pthread_t thread2;
 	
@@ -165,14 +144,12 @@ int test1 (int option, int runlen,int table, char* colname) {
 	else if (option == 3) {
 		tutil.write = true;
 	}
-	// cout << "creating consumer" << endl;
 	pthread_create (&thread2, NULL, consumer, (void *)&tutil);
 
 	BigQ bq (input, output, sortorder, runlen);
 
 	pthread_join (thread1, NULL);
 	pthread_join (thread2, NULL);
-	// cout << "tutil success: "<<tutil.success << endl;
 	return tutil.success;
 }
 // NATION TESTS
