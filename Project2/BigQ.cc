75bae5155354cb82c43315e7f9c742f2461032cb
SethLayton <sethlayton@gmail.com>
2022-03-04 13:46:09 -0500

    All functionality for test1 and test2 complete

--- Project2/BigQ.cc
+++ Project2/BigQ.cc
@@ -8,73 +8,48 @@
 #include <stdlib.h>
 #include <string.h>
 
-BigQ :: BigQ (Pipe &inp, Pipe &outp, OrderMaker &sortorder, int runlen) {	
+BigQ :: BigQ (Pipe &inp, Pipe &outp, OrderMaker &sortorder, int runlen) :in(inp), out(outp) {	
 
-	in = inp;
-	out = outp;
 	so = sortorder;
 	runlength = runlen;
-	cout << "ctor &bigQ " << this << endl;
-	cout << "&in " << &in << endl;
 	threads = pthread_t();
 	//create thread and initialize starting values
 	//bigqutil bqutil = {&inp, &outp, &sortorder, runlen, this}; //pass the thread the bigqutil structure for all the information it needs
 	pthread_create(&threads, NULL, ts, this); //actually create the thread
-	cout << "&threads " << &threads << endl;
-	
 }
 
 BigQ::~BigQ () { 
-	 cout << "dtor called" << endl;
 }
-void* BigQ::pthreadwait () {
-	//cout << "4&bigQ " << this << endl;
-	//cout << "waitingcalled" << endl;
-	//cout << "&threads " << &threads << endl;
+void BigQ::pthreadwait () {
 	pthread_join(threads, NULL);
-	//cout << "waitingcalled??" << endl;
-	return nullptr;
 }
 //thread starter
 void * ts(void *arg)
 {
-	cout << "thread started?" << endl;
   	return reinterpret_cast<BigQ*>(arg)->DoWork();
-	cout << "thread started?---" << endl;
 }
  
 void *BigQ::DoWork() {
-	cout << "do work called" << endl;
-
 	myFile.Open(0, "f_path"); //create our file that stores all the runs
-	//bigqutil *b = (bigqutil *) arg;	//cast our passed structure	
 	Record myRec; //create a record to store read in records from the input pipe
 	std::vector<Record> records; //create a vector to store above records
-	cout << "??" << endl;
 	file_length = myFile.GetLength(); //get the current length of the file
-	cout << "??" << endl;
-	long curSizeInBytes = 0; //init a length variable for total bytes of a run read
+	int curSizeInBytes = 0; //init a length variable for total bytes of a run read
 	int rlen = runlength; 		
-	long maxRunBytes = rlen * PAGE_SIZE; //calculate how long a run can be in bytes
-	//OrderMaker tempOrder = *(b->order); //set our sort order
+	int maxRunBytes = rlen * PAGE_SIZE; //calculate how long a run can be in bytes
 	Compare comparator(&so); //create the comparator used in the vector sort
 	//read in a record from the input pipe
 	while (in.Remove (&myRec)) {
-		cout << "Removed record" << endl;
 		char *bytes = myRec.GetBits(); //get the size of that record in bytes
-		long recBytes = ((long *)bytes)[0]; //convert to an int		
-		//cout << "curSizeInBytes Before: " <<  curSizeInBytes << endl;	
+		int recBytes = ((int *)bytes)[0]; //convert to an int		
 		curSizeInBytes = curSizeInBytes + recBytes; //update the total size of the vector
-		//cout << "recBytes: " <<  recBytes << endl;
-		//cout << "curSizeInBytes After: " <<  curSizeInBytes << endl;
 		//the vector is now full with enough records to fill up a run
 		if (curSizeInBytes > maxRunBytes) {								
 			std::sort(records.begin(),records.end(),comparator); //sort the vector
-
 			//write the records from the vector to pages, and the pages to the file
 			for (auto & currRec : records) {
 				//write record to page (returns 0 if page is full)
-				if (myPage.Append(&currRec) == 0) {						
+				if (myPage.Append(&currRec) == 0) {	
 					myFile.AddPage(&myPage, file_length); //if that page is full, write it to the file
 					file_length++; //update the new length of the file						
 					myPage.EmptyItOut(); //empty the page out
@@ -89,7 +64,7 @@ void *BigQ::DoWork() {
 			//this block handles residuals
 			//write these to the vector for next run processing
 			//only if the number of pages is greater than run length
-			if (pageCount % rlen == 0) {
+			if (myPage.GetNumRecs() > 0) {
 				Record temp; //init a new record
 				//read from the page
 				while (myPage.GetFirst(&temp) != 0) {
@@ -97,6 +72,7 @@ void *BigQ::DoWork() {
 					char *bytes = temp.GetBits();
 					int recBytes = ((int *)bytes)[0];
 					curSizeInBytes = curSizeInBytes + recBytes; //update bytes
+					temp.SetNull();
 				}
 			}
 			//if we get here that means there was a non full page at the end
@@ -112,29 +88,28 @@ void *BigQ::DoWork() {
 		}						
 		records.push_back(myRec); // //add the record to the vector for future sorting	
 		myRec.SetNull(); //deallocate the record to clear residuals
+		
 	}
 	//this block is only hit if we read all the information from the input
 	//pipe and it wasnt enough to fill an entire page
-	if (records.size() > 0){	
-		Schema ms("catalog", "nation");
-		cout << "pre sort" << endl;
-		for (auto & currRec : records) {
-			currRec.Print(&ms);
-		}
-		cout << "post sort" << endl;		
-		std::sort(records.begin(),records.end(),comparator); //sort our vector
-		for (auto & currRec : records) {
-			currRec.Print(&ms);
-		}
+	if (records.size() > 0){					
+		std::sort(records.begin(),records.end(),comparator); //sort our vector		
 		//write each record to the page
+		int totalbytes = 0;
 		for (int i = 0; i < records.size(); i++) {	
-			//write record to page (returns 0 if page is full)			
-			if (myPage.Append(&records[i]) == 0) {					
+			Record temp;
+			temp.Consume(&records[i]);
+			//write record to page (returns 0 if page is full)
+			char *bytes = temp.GetBits(); //get the size of that record in bytes
+			int recBytes = ((int *)bytes)[0]; //convert to an int		
+			totalbytes = totalbytes + recBytes; //update the total size of the vector			
+			if (myPage.Append(&temp) == 0) {				
 				myFile.AddPage(&myPage, file_length); //if that page is full, write it to the file
 				file_length++; //increment file length					
 				myPage.EmptyItOut();//empty the page out
 				pageCount++; //update the total number of pages processed					
-				myPage.Append(&records[i]);	//add the record to the new empty page			
+				myPage.Append(&temp);	//add the record to the new empty page
+				totalbytes = recBytes;			
 			}
 		}
 		//if we looped through the above and didnt get enough records to fill a page
@@ -151,7 +126,6 @@ void *BigQ::DoWork() {
 	//now we need to sort all the runs
 	//This is "Phase 2" of the TPMMS algo
 	FinalSort();
-
 	//shutdown the out pipe
 	out.ShutDown();	
 	pthread_exit(NULL);	
@@ -164,16 +138,13 @@ void BigQ::FinalSort() {
 	off_t totalPages = myFile.GetLength() - 1;
 	int rlen = runlength; 
 	int totalRuns = int(ceil(double(totalPages) / double(rlen)));	
-
 	int offset = rlen;	//set the number of pages in each run
 	Page myPagez[totalRuns]; //initialize an array to hold the pages of the runs	
 	Record PQ[totalRuns]; //initialize our custome priority queue array
 	int pagecount[totalRuns] = { 0 }; //initialize our array for the number of completed pages in a run
 	ComparisonEngine c;	//initialize the comp engine to do the compares for the PQ		
-	int failedPages = 0; //count how many completed pages there are in total
-	
+	int failedPages = 0; //count how many completed pages there are in total	
 	Record tempRecord; //init a record to store records used temporarily
-
 	int run_index = 0; //counts which run we are currently initializing
 	//Loop through one time and grab the first page from each run
 	//Also grab the first record from each of these first runs and put into the PQ
@@ -213,10 +184,12 @@ void BigQ::FinalSort() {
 				//if this run has not completed all of its pages
 				if (pagecount[index_min] < offset) {
 					int newPage_index = (index_min * offset) + pagecount[index_min]; //set the index of the next page in this run	
-					myFile.GetPage(&myPagez[index_min],newPage_index); //read in the next page
-					tempRecord.SetNull(); //deallocate the temprecord incase there is leftovers
-					myPagez[index_min].GetFirst(&tempRecord); //read in the first record from the new page
-					PQ[index_min].Consume(&tempRecord);	//back fill the empty spot in the PQ
+					if (newPage_index < totalPages) {
+						myFile.GetPage(&myPagez[index_min],newPage_index); //read in the next page
+						tempRecord.SetNull(); //deallocate the temprecord incase there is leftovers
+						myPagez[index_min].GetFirst(&tempRecord); //read in the first record from the new page
+						PQ[index_min].Consume(&tempRecord);	//back fill the empty spot in the PQ
+					}					
 				}	
 
 				//Calculates the lowest run that has not been completely exhausted of pages
@@ -231,8 +204,7 @@ void BigQ::FinalSort() {
 		//there are records to read from the current page still
 		else {			
 			PQ[index_min].Consume(&tempRecord); //insert the record in our custom priority queue
-		}
-		
+		}		
 	}
 }
 
