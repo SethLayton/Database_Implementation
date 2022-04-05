c02cbd43750be10046dfba432e0556a95cb5cb50
SethLayton <sethlayton@gmail.com>
2022-04-04 20:06:53 -0400

    updates

--- Project3/test.cc
+++ Project3/test.cc
@@ -95,19 +95,10 @@ void init_SF_c (char *pred_str, int numpgs) {
 // expected output: 31 records
 void q1 () {
 
-	char *pred_ps = "(ps_suppkey=ps_suppkey)";//"(ps_supplycost = 771.64)";
+	char *pred_ps = "(ps_supplycost < 1.04)";
 	init_SF_ps (pred_ps, 100);
 	SelectFile SF_ps(dbf_ps, _ps, cnf_ps, lit_ps, true);
-	//SF_ps.Run (dbf_ps, _ps, cnf_ps, lit_ps);
-	char *fwpath = "ps.test";
-	FILE *writefile = fopen (fwpath, "w");
-	
-	
-
-	
-	WriteOut W(_ps, writefile, *ps->schema());
 	SF_ps.WaitUntilDone ();
-	W.WaitUntilDone();
 
 	int cnt = clear_pipe (_ps, ps->schema (), true);
 	cout << "\n\n query1 returned " << cnt << " records \n";
@@ -442,15 +433,14 @@ possible plan:
 
 int main (int argc, char *argv[]) {
 
-	// if (argc != 2) {
-	// 	cerr << " Usage: ./test.out [1-8] \n";
-	// 	exit (0);
-	// }
+	if (argc != 2) {
+		cerr << " Usage: ./test.out [1-8] \n";
+		exit (0);
+	}
 
 	void (*query_ptr[]) () = {&q1, &q2, &q3, &q4, &q5, &q6, &q7, &q8};  
 	void (*query) ();
-	//int qindx = atoi (argv[1]);
-	int qindx = 6;
+	int qindx = atoi (argv[1]);
 	if (qindx > 0 && qindx < 9) {
 		setup ();
 		query = query_ptr [qindx - 1];
